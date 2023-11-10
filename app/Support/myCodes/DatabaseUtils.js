36bcc9980dabeb6c7da11d6d2fa22411f4744097
chigoox <dikeemmanuel54@gmail.com>
2023-11-09 20:10:38 -0500

    up

--- app/Support/myCodes/DatabaseUtils.js
+++ app/Support/myCodes/DatabaseUtils.js
@@ -14,13 +14,15 @@ updateArrayDatabaseItem('Admin', 'Users', 'allUIDs', UID)
 export const initFollowing = async (user) => {
     try {
         const data = await fetchDocument('Users', user?.uid)
+
+         if (data?.uid == undefined) await addToDatabase('Users', user.uid, 'uid', user.uid)
+            if (data?.uid == '' || undefined) await addToDatabase('Users', user.uid, 'displayName', user.displayName)
+            if (data.followers == undefined) await addToDatabase('Users', user.uid, 'followers', [])
+            if (data?.following == undefined) await  addToDatabase('Users', user.uid, 'following', [])
+            if (data?.donations == undefined) await addToDatabase('Users', user.uid, 'donations', [])
         if(data?.global){
             
-            if (data?.uid == undefined) addToDatabase('Users', user.uid, 'uid', user.uid)
-            if (data?.uid == '' || undefined) addToDatabase('Users', user.uid, 'displayName', user.displayName)
-            if (data.followers == undefined) addToDatabase('Users', user.uid, 'followers', [])
-            if (data?.following == undefined) addToDatabase('Users', user.uid, 'following', [])
-            if (data?.donations == undefined) addToDatabase('Users', user.uid, 'donations', [])
+           
         }
           } catch (error) {
             console.log(error.message)
