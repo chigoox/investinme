9903ae5d32afa0f87e0bddee82e2ec11156aadfb
chigoox <dikeemmanuel54@gmail.com>
2023-12-02 18:23:11 -0500

    update

--- app/META.js
+++ app/META.js
@@ -7,25 +7,27 @@ export const siteEmail = 'em.ed5productions@gmail.com'
 
 export const NavBarVideoURL = '7FjL6i04_uY'
 export const postIDPrefix = 'IVA'
+export const productIDPrefix = 'PID'
+export const orderNumberPrefix = 'SOID'
 export const category = ['Hoodies', 'Shorts']
 
 export const IGFeedURL = 'https://feeds.behold.so/JhZoahEX5QC6npL6BO7Y'
 
 const img1 = 'https://images.unsplash.com/photo-1697644371824-41d4d0a8a12f?auto=format&fit=crop&q=80&w=3871&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
 const img2 = 'https://wwd.com/wp-content/uploads/2016/07/short-shorts-14.jpg?w=1280'
-export const bannerImage = [img1, img2,'']
+export const bannerImage = [img1, img2, '']
 
 
-export const categoryLinks = category.map((category)=>{
+export const categoryLinks = category.map((category) => {
     return category.replace(/\s/g, '')
 })
 
 
-    
-    
-    
-    
-                            
+
+
+
+
+
 
 
 
